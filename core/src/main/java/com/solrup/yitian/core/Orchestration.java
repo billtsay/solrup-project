9508823c1b7ba8dcf3a30d5706683dfd23c2bba1
Bill Tsay <billtsay@gmail.com>
2018-07-14 06:23:07 -0700

    updated

--- core/src/main/java/com/solrup/yitian/core/Orchestration.java
+++ core/src/main/java/com/solrup/yitian/core/Orchestration.java
@@ -3,12 +3,12 @@ package com.solrup.yitian.core;
 public interface Orchestration {
     void initialize(Context context);
     void instantiate(Context context);
-    void materialize(Context context);
+    void activate(Context context);
     void modify(Context context);
     void synchronize(Context context);
     void request(Context context);
     void approve(Context context);
     void persist(Context context);
-    void destantiate(Context context);
+    void inactivate(Context context);
     void delete(Context context);
 }

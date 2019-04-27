1039755f195ed778ac48916d6b788938b04e5157
billtsay <billtsay@gmail.com>
2019-04-27 05:41:46 -0700

    evtgen

--- solrup-evtgen/core/src/main/java/com/solrup/evtgen/env/BluePrintEnvironment.java
+++ solrup-evtgen/core/src/main/java/com/solrup/evtgen/env/BluePrintEnvironment.java
@@ -2,14 +2,15 @@ package com.solrup.evtgen.env;
 
 import com.solrup.evtgen.Environment;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.osgi.framework.BundleContext;
 import org.springframework.context.ApplicationContext;
 
 import java.util.logging.Logger;
 
-public class BluePrintEnvironment implements Environment<ApplicationContext> {
+public class BluePrintEnvironment implements Environment<BundleContext> {
 
     @Override
-    public ApplicationContext context() {
+    public BundleContext context() {
         return null;
     }
 
